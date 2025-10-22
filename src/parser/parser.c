5e8235abf160ef0119c51b2b81a49a001a325b1b
variasdesign <variasdesign@pm.me>
2025-10-22 16:52:14 +0200

    add parser plan comments

---
+++ src/parser/parser.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varias-c <varias-c@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 16:48:09 by varias-c          #+#    #+#             */
+/*   Updated: 2025/10/22 16:50:18 by varias-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// Parser: Iterate args while searching all pointer tables, then link each
+// pointer table occurrence to obtain the command table
+t_cmd	*parser(char args, t_mini *msh)
+{
+}
